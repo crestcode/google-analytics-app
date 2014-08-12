9ecee4536551463172c89abf77bcdbd4d2d65617
John Kasiewicz <crestcode@gmail.com>
2014-08-12 12:48:48 -0400

    add basic reporting view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
    def index
+     @reporting_start_date = params[:reporting_start_date]
+     @reporting_end_date = params[:reporting_end_date]
 
+     google_data = GoogleAnalytics::get_google_data(@reporting_start_date, @reporting_end_date)
+     @visitors = google_data[:visitors]
+     @top_viewed_products = google_data[:top_products]
    end
 end
